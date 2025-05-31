9d4b4230956acef7cd114e5ccfe698c2b403a416
Javier Olveyra <jolveyra@gmail.com>
2025-05-31 20:05:24 -0300

    fix: fixed action

--- scripts/SmokeAction.js
+++ scripts/SmokeAction.js
@@ -3,28 +3,33 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export const options = {
-   iterations: 10, // Define el número de iteraciones que se ejecutarán
+    iterations: 10, // Define el número de iteraciones que se ejecutarán
 };
 
 export default function () {
-  // define URL and payload
-  const url = 'http://localhost:3000/tasks';
-  const payload = JSON.stringify({
-    title: 'default',
-    description: '12345678',
-  });
+    // define URL and payload
+    const url = 'http://localhost:3000/tasks';
+    const payload = JSON.stringify({
+        title: 'default',
+        description: '12345678',
+    });
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
 
-  // send a post request and save response as a variable
-  const res = http.post(url, payload, params);
+    // send a post request and save response as a variable
+    const res = http.post(url, payload, params);
 
-  // Agrega una verificación de que el código sea 200 para el log
-  check(res, {
-    'response code was 201': (res) => res.status == 201,
-  });
+    // Agrega una verificación de que el código sea 200 para el log
+    const succes = check(res, {
+        'response code was 201': (res) => res.status == 201,
+    });
+
+
+    if (!success) {
+        fail('Al menos un check falló, abortando ejecución');
+    }
 }
